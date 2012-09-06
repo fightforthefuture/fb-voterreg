a24f627b51d3d77121e54587d3d0f016f19b41a5
Adam Duston <aduston@gmail.com>
2012-09-05 21:18:05 -0400

    Handling initial POST from FB

--- main/views.py
+++ main/views.py
@@ -1,5 +1,28 @@
+import facebook
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+from django.views.decorators.csrf import csrf_exempt
 
+def _post_index(request):
+    signed_request = request.POST["signed_request"]
+    data = facebook.parse_signed_request(
+        signed_request,
+        settings.FACEBOOK_SECRET_KEY)
+    if not data.get("user_id"):
+        scope = ("user_birthday,user_location,friends_birthday,"
+                 "friends_hometown,friends_location")
+        auth_url = facebook.auth_url(settings.FACEBOOK_APP_ID,
+                                     settings.FACEBOOK_CANVAS_PAGE,
+                                     scope)
+        markup = ('<script type="text/javascript">'
+                  'top.location.href="%s"</script>' % auth_url)
+        return HttpResponse(markup)
+    return None
+
+@csrf_exempt
 def index(request):
+    if request.method == "POST":
+        response = _post_index(request)
+        if response:
+            return response
     return render_to_response("main_index.html", { }, RequestContext(request))
