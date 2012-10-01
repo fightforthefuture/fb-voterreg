62838a5f31271255515e6265c60285f87480b263
Adam Duston <aduston@gmail.com>
2012-10-01 19:17:35 -0400

    Created small mgmt command to blow away individual users. Useful for testing.

--- README.md
+++ README.md
@@ -9,8 +9,17 @@ Want to develop locally? Easy! Here are the steps:
 
 * Add this line to your hosts file:
 
-      127.0.0.1       local.voterreg.org
+        127.0.0.1       local.voterreg.org
 
 * syncdb, migrate, runserver
 * Start celeryd with `./manage.py celeryd --loglevel=INFO`
 * Access https://apps.facebook.com/258722907563918/
+
+## Other nice utilities for developing
+
+* Want to clear one user's data? No problem:
+
+        ./manage.py clearuser "Holmes Wilson"
+
+  Username must match one from Facebook.
+
---
+++ main/management/commands/clearuser.py
@@ -0,0 +1,18 @@
+from django.core.management.base import BaseCommand, CommandError
+from main import models
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        name = args[0]
+        try:
+            user = models.User.objects.get(name=name)
+        except models.User.ObjectDoesNotExist:
+            print("No user named {0} found".format(name))
+            return
+        proceed = raw_input(
+            "This will clear all info for {0}. Sure? (y/n) ".format(name))
+        if proceed != "y":
+            return
+        user.friendship_set.all().delete()
+        user.friendshipbatch_set.all().delete()
+        user.delete()
