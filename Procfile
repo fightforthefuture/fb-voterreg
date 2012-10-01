7114f524d6a356346fc6ab5b86ceefda4e673714
Chuck Harmston <chuck@chuckharmston.com>
2012-10-01 12:41:41 -0500

    Installs newrelic

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: gunicorn voterreg.wsgi -b 0.0.0.0:$PORT
+web: newrelic-admin run-program hellodjango/manage.py run_gunicorn -b "0.0.0.0:$PORT" -w 3
 celeryd: python manage.py celeryd -E -B --loglevel=INFO
--- voterreg/settings.py
+++ voterreg/settings.py
@@ -79,6 +79,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'gunicorn',
     'kombu.transport.django',
     'djcelery',
     'south',
