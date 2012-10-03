2ede170658effb45efec8233ad0a1c728b933938
Adam Duston <aduston@gmail.com>
2012-10-03 10:30:40 -0400

    Now identifying individual users for kissmetrics

--- context_processors.py
+++ context_processors.py
@@ -4,3 +4,7 @@ def add_settings(request):
     return { "FACEBOOK_APP_ID": settings.FACEBOOK_APP_ID,
              "BASE_URL": settings.BASE_URL,
              "KM_CODE": settings.KM_CODE }
+
+def add_fbuid(request):
+    if hasattr(request, "facebook") and request.facebook is not None:
+        return { "my_uid": request.facebook["uid"] }
--- voterreg/settings.py
+++ voterreg/settings.py
@@ -70,7 +70,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.static",
     "django.core.context_processors.tz",
     "django.contrib.messages.context_processors.messages",
-    "context_processors.add_settings" )
+    "context_processors.add_settings",
+    "context_processors.add_fbuid" )
 
 INSTALLED_APPS = (
     'django.contrib.auth',
