0a9326de14a1d56a2078f90aaf462846d732abd2
Chuck Harmston <chuck@chuckharmston.com>
2012-10-04 16:18:04 -0500

    PEP8 fixes to shut up my linter

--- main/views.py
+++ main/views.py
@@ -8,14 +8,13 @@ from django.template.context import RequestContext
 from django.views.decorators.csrf import csrf_exempt
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse
-from fb_friends import get_friends
 from tasks import fetch_fb_friends, update_friends_of
 from decorators import render_json
 from voterapi import fetch_voter_from_fb_profile, correct_voter
 from models import User, FriendshipBatch, BATCH_REGULAR
-from forms import WontVoteForm
 from datetime import datetime
 from fb_utils import FacebookProfile
+
 
 def _post_index(request):
     signed_request = request.POST["signed_request"]
@@ -33,12 +32,14 @@ def _post_index(request):
         return HttpResponse(markup)
     return None
 
+
 def _main_content_context(user):
     return {
         "registered": user.registered,
         "pledged": user.pledged,
         "invited_friends": user.invited_friends
     }
+
 
 def _index_redirect(user):
     if user.wont_vote:
@@ -51,6 +52,7 @@ def _index_redirect(user):
     else:
         return redirect("main:register")
 
+
 def _fetch_fb_friends(request):
     fb_uid = request.facebook["uid"]
     access_token = request.facebook["access_token"]
@@ -60,6 +62,7 @@ def _fetch_fb_friends(request):
         user.update_friends_fetch()
         user.save()
         fetch_fb_friends.delay(fb_uid, access_token)
+
 
 @csrf_exempt
 def index(request):
@@ -71,8 +74,9 @@ def index(request):
     if user.data_fetched:
         return _index_redirect(user)
     return render_to_response(
-        "loading.html", 
+        "loading.html",
         context_instance=RequestContext(request))
+
 
 def fetch_me(request):
     user = User.objects.get(fb_uid=request.facebook["uid"])
@@ -102,10 +106,11 @@ def fetch_me(request):
         else reverse("main:register")
     return HttpResponse(redirect_url, content_type="text/plain")
 
+
 def _friend_listing_page(request, template, additional_context={}, user=None):
     if not user:
         user = User.objects.get(fb_uid=request.facebook["uid"])
-    context = { "user": user }
+    context = {"user": user}
     context.update(additional_context)
     if user.friendship_set.filter(registered=True).count() >= 4:
         context["friends"] = user.friendship_set.order_by("-display_ordering")[:4]
@@ -116,9 +121,13 @@ def _friend_listing_page(request, template, additional_context={}, user=None):
         context,
         context_instance=RequestContext(request))
 
+
 def register(request):
     user = User.objects.get(fb_uid=request.facebook["uid"])
-    context = { "name": user.name, "page": "register" }
+    context = {
+        "name": user.name,
+        "page": "register"
+    }
     if user.location_city:
         context["location"] = "{0}, {1}".format(
             user.location_city, user.location_state)
@@ -126,6 +135,7 @@ def register(request):
         context["birthday"] = user.birthday.strftime("%b %d, %Y")
     return _friend_listing_page(
         request, "register.html", additional_context=context, user=user)
+
 
 def pledge(request):
     user = User.objects.get(fb_uid=request.facebook["uid"])
@@ -140,7 +150,8 @@ def pledge(request):
             "Thank you for registering to vote!")
     return _friend_listing_page(
         request, "pledge.html",
-        additional_context={ "page": "pledge" })
+        additional_context={"page": "pledge"})
+
 
 def invite_friends(request):
     user = User.objects.get(fb_uid=request.facebook["uid"])
@@ -153,11 +164,12 @@ def invite_friends(request):
         "uninvited_batches": user.friendshipbatch_set.filter(
             completely_fetched=True, invite_date__isnull=True),
         "still_loading": not user.friends_fetched,
-        "my_city": user.location_city }
+        "my_city": user.location_city}
     return render_to_response(
         "invite_friends.html",
         context,
         context_instance=RequestContext(request))
+
 
 @render_json
 def submit_pledge(request):
@@ -169,20 +181,22 @@ def submit_pledge(request):
     messages.add_message(
         request, messages.INFO,
         "Thank you for pledging to vote!")
-    return { "next": reverse("main:invite_friends") }
+    return {"next": reverse("main:invite_friends")}
+
 
 @render_json
 def fetch_friends(request):
     user = User.objects.get(fb_uid=request.facebook["uid"])
     if user.friendship_set.filter(registered=True).count() < 4:
-        return { "fetched": False }
+        return {"fetched": False}
     friends = user.friendship_set.order_by("-display_ordering")[:4]
     html = render_to_string(
         "_friends.html",
-        { "friends": friends },
+        {"friends": friends},
         context_instance=RequestContext(request))
-    return { "fetched": True,
-             "html": html }
+    return {"fetched": True,
+             "html": html}
+
 
 @render_json
 def wont_vote(request):
@@ -190,9 +204,10 @@ def wont_vote(request):
     user.wont_vote_reason = "rather_not_say"
     user.save()
     messages.add_message(
-        request, messages.INFO, 
+        request, messages.INFO,
         "Thank you! Even though you can't vote, you can still invite your friends.")
-    return { "next": reverse("main:invite_friends") }
+    return {"next": reverse("main:invite_friends")}
+
 
 @render_json
 def im_actually_registered(request):
@@ -203,7 +218,7 @@ def im_actually_registered(request):
     messages.add_message(
         request, messages.INFO,
         "You're now marked as registered to vote.")
-    return { "next": reverse("main:pledge") }
+    return {"next": reverse("main:pledge")}
 
 
 @render_json
@@ -223,7 +238,7 @@ def fetch_updated_batches(request):
         if batch.id not in batch_ids:
             htmls.append(
                 render_to_string(
-                    "_batch.html", { "batch": batch }, 
+                    "_batch.html", {"batch": batch},
                     context_instance=RequestContext(request)))
     return {
         "num_registered": f_mgr.filter(registered=True).count(),
@@ -231,7 +246,8 @@ def fetch_updated_batches(request):
         "num_friends": user.num_friends or 0,
         "num_processed": f_mgr.all().count(),
         "boxes": htmls,
-        "finished": user.friends_fetched }
+        "finished": user.friends_fetched}
+
 
 @render_json
 def mark_batch_invited(request):
@@ -239,7 +255,8 @@ def mark_batch_invited(request):
     batch = FriendshipBatch.objects.get(id=batch_id)
     batch.invite_date = datetime.now()
     batch.save()
-    return { "response": "ok" }
+    return {"response": "ok"}
+
 
 def unregistered_friends_list(request):
     user = User.objects.get(fb_uid=request.facebook["uid"])
@@ -250,14 +267,15 @@ def unregistered_friends_list(request):
         friendships.extend(list(batch.friendship_set.all()))
     return render_to_response(
         "unregistered_friends_list.html",
-        { "friendships": friendships },
+        {"friendships": friendships},
         context_instance=RequestContext(request))
+
 
 def register_widget(request):
     user = User.objects.get(fb_uid=request.facebook["uid"])
-    widget_qs = { 
+    widget_qs = {
         "first_name": user.first_name,
-        "last_name": user.last_name }
+        "last_name": user.last_name}
     if user.email:
         widget_qs["email"] = user.email
     if user.location_city:
@@ -269,6 +287,6 @@ def register_widget(request):
         widget_qs["dob_year"] = user.birthday.year
     return render_to_response(
         "register_widget.html",
-        { "widget_qs": urllib.urlencode(widget_qs),
-          "page": "register" },
+        {"widget_qs": urllib.urlencode(widget_qs),
+          "page": "register"},
         context_instance=RequestContext(request))
