a27c6eeabf9a26507150de079d9c1663c005d885
Adam Duston <aduston@gmail.com>
2012-10-05 19:21:47 -0400

    Should be handling Safari users correctly now.

--- middleware.py
+++ middleware.py
@@ -28,29 +28,38 @@ class FacebookMiddleware(object):
         return fb_user
 
     def _get_fb_user(self, request):
+        if request.POST.get("safari", False):
+            fb_user = { "method": "safari",
+                        "uid": request.POST["uid"],
+                        "access_token": request.POST["access_token"] }
+            return fb_user
         fb_user = self._get_fb_user_cookie(request)
         if fb_user:
             return fb_user
         return self._get_fb_user_canvas(request)
+
+    def _is_initial_safari_post(self, request):
+        if 'fb_user' not in request.session and \
+                'safari' not in request.POST:
+            ua = request.META['HTTP_USER_AGENT']
+            if 'Safari' in ua and not 'Chrome' in ua:
+                return True
+        return False
 
     def process_request(self, request):
         fb_user = self._get_fb_user(request)
         request.facebook = fb_user
         if fb_user:
             user, created = User.objects.get_or_create(fb_uid=fb_user["uid"])
-
             # Safari blocks third-party cookies by default.
-            if 'fb_user' not in request.session and 'safari' not in request.POST:
-                ua = request.META['HTTP_USER_AGENT']
-                if 'Safari' in ua and not 'Chrome' in ua:
-                    return render_to_response('safari.html', {
-                        'fb_uid': fb_user['uid'],
-                        'signed_request': request.POST['signed_request'],
-                        'access_token': fb_user['access_token']
-                    }, RequestContext(request))
-
+            if self._is_initial_safari_post(request):
+                return render_to_response(
+                    'safari.html', 
+                    { 'fb_uid': fb_user['uid'],
+                      'signed_request': request.POST['signed_request'],
+                      'access_token': fb_user['access_token']}, 
+                    RequestContext(request))
             request.session["fb_user"] = fb_user
-            print 'setting fb_user session var'
             request.session.modified = True
 
         request.facebook = request.session.get("fb_user", None)
