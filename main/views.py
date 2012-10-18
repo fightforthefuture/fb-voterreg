f8725e746a2e71093815c7cea247b01434e3c6e9
Chuck Harmston <chuck@chuckharmston.com>
2012-10-18 09:43:55 -0500

    Fixes bug in my vote redirection scheme

--- main/views.py
+++ main/views.py
@@ -129,16 +129,21 @@ def my_vote(request):
     user = User.objects.get(fb_uid=request.facebook["uid"])
     if user.pledged and user.registered:
 
+        # The user has explicitly navigated here from the navigation, so don't
+        # try to guess what they wanted.
+        if 'nav' in request.GET:
+            return redirect('main:my_vote_vote')
+
         # If the user just completed the form in the my_vote_pledge view, then
-        # send them to the 'Have you voted?' form. Also do so if they
-        # explicitly navigated there via 'My Vote'. If not, send them to the
+        # send them to the 'Have you voted?' form. If not, send them to the
         # invite friends page.
         referer = request.META.get('HTTP_REFERER', None)
         if referer:
             referer_path = urlparse(referer)[2]
             view_name = resolve(referer_path).view_name
-            if view_name == 'main:my_vote_pledge' or 'nav' in request.GET:
+            if view_name == 'main:my_vote_pledge':
                 return redirect('main:my_vote_vote')
+
         return redirect('main:invite_friends_2')
 
     elif user.registered:
