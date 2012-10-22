f5cc938bad48696f5fd65e2c651ca9b070f022d0
Chuck Harmston <chuck@chuckharmston.com>
2012-10-22 16:06:40 -0500

    Adds error pages

--- voterreg/urls.py
+++ voterreg/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 from main.views import OGObjectView
 
@@ -15,4 +16,12 @@ urlpatterns = patterns('',
     url(r'^rosetta/', include('rosetta.urls')),
     url(r'^share_pledge', OGObjectView.as_view(template_name='pledge/index.html'), name="pledge_object"),
     url(r'^share_vote', OGObjectView.as_view(template_name='vote/index.html'), name="vote_object"),
-) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        url(r'^404', TemplateView.as_view(template_name='404.html')),
+        url(r'^500', TemplateView.as_view(template_name='500.html')),
+    )
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
